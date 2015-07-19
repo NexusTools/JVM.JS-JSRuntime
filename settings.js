a39d1499c26514249515505cfadb06a6373a45c9
Katelyn Slater <ktaeyln@gmail.com>
2015-07-19 11:28:16 -0400

    Initial commit

---
+++ settings.js
@@ -0,0 +1,14 @@
+(function(JVM) {
+    JVM.settings = {
+        convertStack: true,
+        dumpImpl: false,
+        dumpStack: false,
+        dumpSource: false,
+        dumpOptimizations: false,
+        verbose: true,
+
+        // Optimizations
+        allowOpt: true,
+        optPasses: 1
+    };
+})(JVM);
